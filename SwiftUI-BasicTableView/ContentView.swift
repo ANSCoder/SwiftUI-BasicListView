6485947ad2c2858fd8e06e026a32d5eecb38dec1
ANSCoder <nimjea@gmail.com>
2019-10-13 19:20:28 +0530

    Initial Commit

---
+++ SwiftUI-BasicTableView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-BasicTableView
+//
+//  Created by Anand Nimje on 13/10/19.
+//  Copyright © 2019 Anand. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
